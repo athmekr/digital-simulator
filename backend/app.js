d2c497c46ab89403c0663907aed1c99d16b8ba82
athmekr <thanosmkr@gmail.com>
2020-07-23 16:07:24 +0300

    Fetching dummy data

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,24 @@ const express = require ('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middlware');
-  next();
-});
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+      id:'thanos',
+      title: 'First server-side post',
+      content: 'this is coming from the server'
+    },
+    {
+      id:'aek',
+      title: 'Second server-side post',
+      content: 'this is coming from the server!'
+    }
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
