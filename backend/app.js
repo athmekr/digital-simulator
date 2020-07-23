f843c22da5120657dfd36f4dde9c47e8966c74ee
athmekr <thanosmkr@gmail.com>
2020-07-23 17:04:25 +0300

    Added POST backend point

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
-const e = require("express");
-
 const express = require ('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin','*');
@@ -10,6 +11,14 @@ app.use((req, res, next) => {
   'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE, OPTIONS');
   next();
+});
+
+app.post('/api/posts',(req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
 });
 
 app.use('/api/posts',(req, res, next) => {
@@ -27,7 +36,7 @@ app.use('/api/posts',(req, res, next) => {
   ];
 
   res.status(200).json({
-    message: 'Posts fetched succesfully!',
+    message: 'Posts fetched successfully!',
     posts: posts
   });
 });
