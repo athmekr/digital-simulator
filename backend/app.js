af9878c4b71a0b604483ba47659281967ea9c022
athmekr <thanosmkr@gmail.com>
2020-07-23 14:05:58 +0300

    Installed express, created app.js, modified server.js

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const e = require("express");
+
+const express = require ('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('First middlware');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send('Hello from express!');
+});
+
+module.exports = app;
