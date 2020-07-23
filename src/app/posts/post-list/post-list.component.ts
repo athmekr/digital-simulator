0cd0eb8525e0a1bbd6a766286bcdc207e50a332c
athmekr <thanosmkr@gmail.com>
2020-07-23 11:21:44 +0300

    Added onDestroy and getPostUpdateListener

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Subscription} from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service'
@@ -10,13 +11,20 @@ import { PostsService } from '../posts.service'
   styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit(): void {
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
   }
 }
