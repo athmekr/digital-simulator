e40886ecb8ad38dc323afb7864fcba2bb78c957d
athmekr <athmekr@gmail.com>
2020-08-11 13:19:38 +0300

    Installed multer for file upload to backend

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -73,7 +73,8 @@ export class PostCreateComponent implements OnInit {
     if (this.mode === "create"){
       this.postsService.addPost(
         this.form.value.title,
-        this.form.value.content
+        this.form.value.content,
+        this.form.value.image
       );
     } else {
       this.postsService.updatePost(
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -38,12 +38,20 @@ export class PostsService {
     ('http://localhost:3000/api/posts/'+ id);
   }
 
-  addPost(title: string, content: string){
-    const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
+  addPost(title: string, content: string, image: File){
+    //const post: Post = {id: null, title: title, content: content};
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
+
+    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', postData)
         .subscribe((responseData) => {
-          const id = responseData.postId;
-          post.id = id;
+          const post: Post = {
+            id: responseData.postId,
+            title: title,
+            content: content
+          };
           this.posts.push(post); //updating local data
           this.postsUpdated.next([...this.posts]);
           this.router.navigate(['/']);
