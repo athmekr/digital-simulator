0d65b0a406939d980607dbcb7681ee9dc0f979c3
athmekr <athmekr@gmail.com>
2020-08-11 15:13:30 +0300

    Finished uploading images to backend and server

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require ('path');
 const express = require ('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -15,6 +16,8 @@ mongoose.connect('mongodb+srv://thanos:TRuUtEDejPXE0432@cluster0.60cyg.mongodb.n
   });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin','*');
