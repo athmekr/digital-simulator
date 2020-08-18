e65ef0bd075de35183d5210f1b0043bb91d0ded0
athmekr <athmekr@gmail.com>
2020-08-18 13:57:49 +0300

    Adding user id to posts

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -2,9 +2,10 @@ const jwt = require ('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, 'Secret_this_should_be_longer')
-    next();
+      const token = req.headers.authorization.split(' ')[1];
+      const decodedToken = jwt.verify(token, 'Secret_this_should_be_longer');
+      req.userData = {email: decodedToken.email, userId: decodedToken.userId};
+      next();
     } catch (error) {
         res.status(401).json({message: 'Auth failed hard!'});
     }
